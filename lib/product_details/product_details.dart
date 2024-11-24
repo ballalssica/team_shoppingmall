603f6a02122eb9092bdb734c54e1ebdb18c4a51c
RYUjeonghyeon911 <jeonghyeonairking@gmail.com>
2024-11-24 15:31:41 +0900

    틀만 만듬
    틀만만듬

--- lib/product_details/product_details.dart
+++ lib/product_details/product_details.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatelessWidget {
-  const ProductDetails({super.key});
+  const ProductDetails({super.key}); // const 생성자 추가
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('상품상세'), // const 추가
+        centerTitle: true,
+        title: Text('상품 상세 정보'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
-      body: const Center(
-        child: Text(
-          'Hello!',
-          style: TextStyle(
-            fontFamily: 'NotoSans', // 'NotoSnas' -> 'NotoSans'로 수정
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '상품 이름: 신선아삭 당근 1kg',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '가격: 9,900원',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '설명: 신선한 당근 한 묶음입니다. 산지 직송으로 하루만에 배달하는 핫당근이에요.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
         ),
       ),
     );
